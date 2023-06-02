727910b21278ddc44ba30032bbeea55282e56071
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-06-02 16:44:51 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      const MyApp(),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        // <== définit le thème Light
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        // <== définit le thème Dark
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.dark, // <== on choisit le Dark
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyApp'),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
