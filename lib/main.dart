c9aa81c4d50b804adf2116c8e8b0b83730392393
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-06-15 02:42:29 +0200

    Boîte d'un choix de quiz terminé!

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // <== définit le thème Light
         brightness: Brightness.light,
@@ -31,37 +32,59 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('QuizWiz', style: TextStyle(color: Colors.white, fontSize: 24)),
+        title: const Text('QuizWiz', style: TextStyle(color: Colors.white, fontSize: 24), textAlign: TextAlign.center),
         backgroundColor: const Color.fromARGB(206, 237, 66, 100),
       ),
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('../assets/background_mainpage.jpg'),
-            fit: BoxFit.contain,
+            fit: BoxFit.cover,
           ),
         ),
-          child: Center(
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 0, 121, 191),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                children: const [
-                  Text(
-                    'Contenu de l\'application',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                  SizedBox(height: 30, width: 8),
-                  Text(
-                    'Description du contenu',
-                    style: TextStyle(color: Colors.white, fontSize: 14),
-                  ),
+        child: Center(
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 20), // Ajouter une marge supérieure
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start, // Aligner les enfants en haut
+              children: const [
+                BoxQuiz(),
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class BoxQuiz extends StatelessWidget {
+  const BoxQuiz({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 0, 121, 191), // Utiliser la couleur hexadécimale spécifiée
+        borderRadius: BorderRadius.circular(10),
+      ),            
+      width: 300,
+      height: 100,
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text(
+            'Minecraft',
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Un phénomène mondial qui a marqué toute une génération de joueurs!.',
+            style: TextStyle(color: Colors.white, fontSize: 14),
+          ),
+        ],
       ),
     );
   }
@@ -77,3 +100,5 @@ class HomePage extends StatelessWidget {
           //   horizontal: 100,
           //   vertical: 70,
           // ),
+
+          // const Color.fromARGB(255, 0, 121, 191),
