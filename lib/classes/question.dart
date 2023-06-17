02565e05f9604cdf5b18d27e568a29f138d5ac9f
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2023-06-17 19:19:31 +0200

    Mise à jour de la classe question

--- lib/classes/question.dart
+++ lib/classes/question.dart
@@ -1,13 +1,19 @@
+// ignore_for_file: prefer_is_empty
+
 class Question {
   final int id;
   final String question;
   final String type;
-  final List<String> answers;
+  List<String> answers;
   
   Question(
     this.id,
     this.question,
     this.type,
-    this.answers, 
-  );
+    this.answers,
+  )  : assert(answers.length >= 1, 'La question doit avoir au moins une réponse.'),
+      assert(type == 'QCM' && answers.length == 4 || type == 'RC' && answers.length >= 1 && answers.length <= 2,
+            'Le nombre de réponses ne correspond pas au type de question.') {
+              answers = answers;
+              }
 }
